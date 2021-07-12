1fcc22fc96ccc6a52f856a7905312121b7cff830
姜彦汐 <jiangyanxi@live.com>
2021-07-12 22:22:11 +0800

    up

--- common/utils.js
+++ common/utils.js
@@ -35,6 +35,41 @@ XEUtils.mixin({
 			return `${dateDiff.yyyy}年之前`
 		}
 		return diffDate
+	},
+	loadResources(array, callback) {
+		const head = document.getElementsByTagName('head')[0];
+		const loader = function(src, handler) {
+			const exist = document.getElementById(src)
+			if (exist) {
+				handler()
+			} else {
+				const type = src.substr(src.lastIndexOf('.') + 1)
+				let resource = document.createElement('js' == type ? 'script' : 'link');
+				resource.id = src
+				if ('js' == type) {
+					resource.src = src
+				} else {
+					resource.media = 'all';
+					resource.href = src;
+					if ('css' == type) {
+						resource.rel = 'stylesheet';
+						resource.type = 'text/css';
+					}
+				}
+				resource.onload = resource.onreadystatechange = function() {
+						resource.onreadystatechange = resource.onload = null;
+						handler();
+					}
+					(head || document.body).appendChild(resource);
+			}
+		};
+		(function run() {
+			if (array.length != 0) {
+				loader(array.shift(), run);
+			} else {
+				callback && callback();
+			}
+		})();
 	}
 })
 
