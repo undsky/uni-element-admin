826f5fb97c659041b505ec9b4ef3efb3991874be
姜彦汐 <jiangyanxi@live.com>
2021-07-13 08:43:01 +0800

    up

--- App.vue
+++ App.vue
@@ -8,12 +8,13 @@
 					this.globalData.isXS = matches
 				}
 			);
+			console.log('App Launch')
 		},
 		onShow: function() {
-
+			console.log('App Show')
 		},
 		onHide: function() {
-
+			console.log('App Hide')
 		}
 	}
 </script>
