fd6893da32a2c45ac2c2811afdce056b75aee337
姜彦汐 <jiangyanxi@live.com>
2021-07-13 11:28:48 +0800

    up

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,21 +1,43 @@
 <template>
-	<view>v{{ version }}</view>
+	<view class="padding">
+		<h3>ID Card</h3>
+		<view>visitorId：{{visitorId}}</view>
+		<view>ip：{{ip}}</view>
+		<view>token：{{token}}</view>
+		<view>version：{{version}}</view>
+	</view>
 </template>
 
 <script>
-export default {
-	name: 'index',
-	data() {
-		return {
-			version: ''
-		};
-	},
-	methods: {},
-	onLoad: async function() {
-		const result = await this.$http.post('/api/version');
-		this.version = result.version;
-	}
-};
+	import FingerprintJS from '@fingerprintjs/fingerprintjs';
+
+	export default {
+		name: 'index',
+		data() {
+			return {
+				visitorId: '',
+				ip: '',
+				token: '',
+				version: ''
+			};
+		},
+		methods: {},
+		onLoad: async function() {
+			const {
+				ip,
+				version
+			} = await this.$http.post('/api/version');
+			this.ip = ip;
+			this.version = version;
+
+			this.$nextTick(async () => {
+				const fpPromise = FingerprintJS.load()
+				const fp = await fpPromise
+				const result = await fp.get()
+				this.visitorId = result.visitorId
+			})
+		}
+	};
 </script>
 
 <style></style>
