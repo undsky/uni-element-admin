6ef89f05cb819ee995229b03e7c1e8a42ee29f6c
姜彦汐 <jiangyanxi@live.com>
2021-07-26 17:14:27 +0800

    up

--- pages/demo/table/table.vue
+++ pages/demo/table/table.vue
@@ -1,18 +1,18 @@
 <template>
-	<view>
-		
-	</view>
+	<scroll-view scroll-y="true" class="main">
+
+	</scroll-view>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
