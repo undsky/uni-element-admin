ffdbe73caf3fc1bafa037f967d0211770062dce7
姜彦汐 <jiangyanxi@live.com>
2021-07-27 14:36:57 +0800

    up

--- pages/demo/image/image.vue
+++ pages/demo/image/image.vue
@@ -1,5 +1,7 @@
 <template>
 	<scroll-view class="main" scroll-y>
+		<VueCropper ref="cropper" img="https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg" autoCrop>
+		</VueCropper>
 		<el-image v-for="(url,idx) in urls" :key="url" :src="url" @click="index = idx"></el-image>
 		<CoolLightBox :items="urls" :index="index" @close="index = null">
 		</CoolLightBox>
@@ -9,10 +11,14 @@
 <script>
 	import CoolLightBox from 'vue-cool-lightbox'
 	import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
+	import {
+		VueCropper
+	} from 'vue-cropper'
 
 	export default {
 		components: {
 			CoolLightBox,
+			VueCropper
 		},
 		data() {
 			return {
@@ -22,8 +28,7 @@
 					'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
 					'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
 					'https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg',
-					'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg',
-					'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg'
+					'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg'
 				],
 				index: null
 			}
