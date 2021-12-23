9b103cfcccdc5a8e9ed1bddac8baf5ffd1149cf9
姜彦汐 <jiangyanxi@live.com>
2021-12-23 11:12:37 +0800

    up

--- pages/demo/aliyun/aliyun.vue
+++ pages/demo/aliyun/aliyun.vue
@@ -1,22 +1,38 @@
 <template>
 	<view>
 		<el-divider content-position="left">OSS 对象存储</el-divider>
+		<el-upload action="" :http-request="handleUpload"><el-button size="small" type="primary">点击上传</el-button></el-upload>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
+import aliyun from '../../../common/aliyun.js';
+
+export default {
+	data() {
+		return {
+			oss: null
+		};
+	},
+	methods: {
+		async handleUpload(option) {
+			console.log(option);
+			const { uid, name, size, type } = option.file;
+			try {
+				await this.oss.upload(`${uid}_${name}`, option.file, {
+					progress: percent => {
+						option.onProgress(percent * 100);
+					}
+				});
+			} catch (e) {
+				console.log(e);
 			}
-		},
-		methods: {
-			
 		}
+	},
+	onLoad: async function(options) {
+		this.oss = await aliyun.OSS('assttest');
 	}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
