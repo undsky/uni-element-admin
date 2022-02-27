23cedaede0e57e43e7e1ff125bcb7ffeb03623ff
姜彦汐 <jiangyanxi@live.com>
2022-02-27 19:33:18 +0800

    up

--- pages/system/role/role.vue
+++ pages/system/role/role.vue
@@ -1,23 +1,22 @@
 <template>
 	<el-row gutter="20">
-		<el-col span="8"></el-col>
+		<el-col span="8">
+			<vxe-table ref="xTree" :tree-config="{ transform: true, rowField: 'id', parentField: 'pid' }" resizable>
+				<vxe-column field="name" title="角色"></vxe-column>
+				<vxe-column field="uname" title="创建人"></vxe-column>
+			</vxe-table>
+		</el-col>
 		<el-col span="16"></el-col>
 	</el-row>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-
-			}
-		},
-		methods: {
-
-		}
-	}
+export default {
+	data() {
+		return {};
+	},
+	methods: {}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
