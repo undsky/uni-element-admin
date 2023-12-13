5302d78f49ab4bd146eb818f8e92ad2412cb07bd
jiangyanxi <jiangyanxi@live.com>
2023-12-13 16:46:11 +0800

    修改压缩配置

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,26 @@
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
-const TransformPages = require('uni-read-pages')
-const {
-	webpack
-} = new TransformPages()
+const TransformPages = require('uni-read-pages');
+const { webpack } = new TransformPages();
 
 module.exports = {
-	configureWebpack: {
-		plugins: [
-			new webpack.DefinePlugin({
-				ROUTES: webpack.DefinePlugin.runtimeValue(() => {
-					const tfPages = new TransformPages({
-						includes: ['path', 'name', 'aliasPath']
-					});
-					return JSON.stringify(tfPages.routes)
-				}, true)
-			}),
-			new CompressionWebpackPlugin({
-				filename: "[path][base].gz",
-				algorithm: "gzip",
-				test: /\.js$/,
-				threshold: 512000,
-				minRatio: 0.8,
-				exclude: /node_modules/,
-			})
-		]
-	}
-}
+    configureWebpack: {
+        plugins: [
+            new webpack.DefinePlugin({
+                ROUTES: webpack.DefinePlugin.runtimeValue(() => {
+                    const tfPages = new TransformPages({
+                        includes: ['path', 'name', 'aliasPath']
+                    });
+                    return JSON.stringify(tfPages.routes);
+                }, true)
+            }),
+            new CompressionWebpackPlugin({
+                filename: '[path][base].gz',
+                algorithm: 'gzip',
+                test: /\.js$/,
+                // threshold: 512000,
+                minRatio: 0.8,
+                exclude: /node_modules/
+            })
+        ]
+    }
+};
